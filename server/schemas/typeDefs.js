875afa277b117c40f52e7ce494d7e1ff58083be2
Jacob Parris <typefortyproductions@gmail.com>
2022-12-19 19:57:53 -0500

    typeDefs Fix and completion, well, only for User typeDef, soooo still work to do but now not the problem

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,59 +1,81 @@
 const { gql } = require("apollo-server-express");
 
+
+//Removed TypeDefs while Debugging
+/*   type access_levels {
+    _id: ID!
+    admin: Boolean
+    staff: Boolean
+    steer: Boolean
+    hoplot: Boolean
+    plotxo: Boolean
+    plot: Boolean
+    npc: Boolean
+    player: Boolean
+    new_user: Boolean
+
+    type species {
+    _id: ID!
+    spec_id: Int
+    species: String!
+    description: String!
+    body_adjust: Int
+    body_per_level_adjust: Int
+    max_body: Int
+    skill_adjust: Int
+    origin: String!
+    makeup_req: String!
+  }
+  } 
+  
+  type char_class {
+    _id: ID!
+    class_id: Int
+    class_name: String!
+    description: String!
+    start_body: Int
+    body_a_level: Int
+    max_body: Int
+    cost_list: Int
+    max_armor: Int
+  }
+
+  //need to add second half of TypeDefs as well as
+// Skill Schema, Item Schema, Spell Schema
+  
+  */
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID!
     username: String!
     email: String!
     password: String!
-    access_level: Int!
-    player_gob: Int!
+    access_level: Int
+    player_gob: Int
     oop_notes: [String]
     characters: [String]
-}
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     profile: User
-}
+  }
 
-type access_levels {
-    _id: ID!
-    admin: Boolean!
-    staff: Boolean!
-    steer: Boolean!
-    hoplot: Boolean!
-    plotxo: Boolean!
-    plot: Boolean!
-    npc: Boolean!
-    player: Boolean!
-    new_user: Boolean!
-}
+  type Query {
+  users: [User]!
+  profile(profileId: ID!): User
+  me: User
+  }
 
-type species {
-    _id: ID!
-    spec_id: Int!
-    species: String!
-    description: String!
-    body_adjust: Int!
-    body_per_level_adjust: Int!
-    max_body: Int!
-    skill_adjust: Int!
-    origin: String!
-    makeup_req: String!
-}
+  type Mutation {
+    addUser(
+        username: String!
+        email: String!
+        password: String!
+        ): Auth
+        
+        login(email: String!, password: String!): Auth
+    }
+    `;
 
-type char_class {
-    _id: ID!
-    class_id: Int!
-    class_name: String!
-    description: String!
-    start_body: Int!
-    body_a_level: Int!
-    max_body: Int!
-    cost_list: Int!
-    max_armor: Int!
-}`
-
-//need to add second half of TypeDefs as well as
-// Skill Schema, Item Schema, Spell Schema
+module.exports = typeDefs;
