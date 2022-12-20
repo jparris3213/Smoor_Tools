e0aa8d87590be78608bee85c12619afd84f39133
Jacob Parris <typefortyproductions@gmail.com>
2022-12-20 01:04:10 -0500

    MongoDB Environnment issue I think

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/smoor_tools",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    }
-);
+const URI = process.env.MONGODB_URL;
 
+console.log(URI)
+
+mongoose.connect(URI, {
+
+useNewUrlParser: true, 
+
+useUnifiedTopology: true 
+
+}, err => {
+if(err) throw err;
+console.log('Connected to MongoDB!!!')
+});
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 //package imports
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
 const path = require("path");
-const { ApolloServer } = require("apollo-server-express");
+
 
 //DB connection
 const db = require("./config/connection");
@@ -10,33 +11,60 @@ const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
-//express initialization and PORT settings
-const app = express();
+// //express initialization and PORT settings
+// const app = express();
 const PORT = process.env.PORT || 3001;
 
 //set up Apollo Server as a new Class
 
-const server = new ApolloServer({
+
+async function startApolloServer(typeDefs, resolvers) {
+    // Same ApolloServer initialization as before
+    const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware, });
+  
+    // Required logic for integrating with Express
+    await server.start();
+  
+    const app = express();
+  
+    server.applyMiddleware({
+       app,
+  
+       // By default, apollo-server hosts its GraphQL endpoint at the
+       // server root. However, *other* Apollo Server packages host it at
+       // /graphql. Optionally provide this to match apollo-server.
+       path: '/'
+    });
+  
+    //check for production status, if in production serve client/build as static assets
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static.apply(path.join(__dirname, "../client/build")));
+    };
+
+    //display base webpage from build
+    app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, 'build','index.html'));
+    });
+    // Modified server startup
+    await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  }
+
+/* const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
-});
-
+}); */
+/* 
 //apply middleware
 server.applyMiddleware( { app });
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); */
 
-//check for production status, if in production serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static.apply(path.join(__dirname, "../client/build")));
-};
 
-//display base webpage from build
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build','index.html'));
-  });
+
+
 
 //Start server and GraphQL (in theory, I'm a little shaky on GraphQL)
 
@@ -46,3 +74,5 @@ db.once("open", () => {
       console.log(`Use GraphQL...theoretically at http://localhost:${PORT}${server.graphqlPath}`);
   })
 })
+
+
